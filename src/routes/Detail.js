b6e040e08ef97e11a5b0dfa782d49d6bb7a41743
gwonsiyun <tldbs0813@gmail.com>
2022-06-16 14:41:05 +0900

    movieApp nomardccoder ReactJS movie wep 7.7

---
+++ src/routes/Detail.js
@@ -0,0 +1,31 @@
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+
+function Detail(){
+    const {id} = useParams();
+    const [loading, setLoading] = useState(true);
+    const [movie, setMovie] = useState();
+    const getMovie = async () => {
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        setMovie(json.data.movie);
+        setLoading(false);
+    }
+    useEffect(()=>{
+        getMovie()
+    },[]);
+    return (
+        <>
+        {loading ? <h1>Loading...</h1> :
+            <>
+                <img src={movie.medium_cover_image}/>
+                <div></div>
+                <h1>{movie.title_long}</h1>
+            </>
+        }
+        </>
+
+    );
+}
+export default Detail;
